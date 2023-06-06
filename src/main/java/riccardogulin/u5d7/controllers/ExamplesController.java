926bb248bce8f84c1fb49653a33a8aa02ccf2f76
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-06-06 11:13:49 +0200

    Adds additional examples

--- src/main/java/riccardogulin/u5d7/controllers/ExamplesController.java
+++ src/main/java/riccardogulin/u5d7/controllers/ExamplesController.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import riccardogulin.u5d7.entities.User;
@@ -48,7 +54,7 @@ public class ExamplesController {
 		users.add(new User(1, "Aldo", "Baglio"));
 		users.add(new User(2, "Giovanni", "Storti"));
 		users.add(new User(3, "Giacomo", "Poretti"));
-		return users;
+		return users; // users verrà convertito automaticamente in Json
 	}
 
 	@GetMapping("/users/{id}")
@@ -66,7 +72,29 @@ public class ExamplesController {
 			}
 		}
 
-		return Optional.ofNullable(u);
+		return Optional.ofNullable(u); // user/null verrà convertito automaticamente in Json
+
+	}
+
+	@PostMapping("/users")
+	@ResponseStatus(HttpStatus.CREATED)
+	public User createUser(@RequestBody User user) {
+		user.setId(4);
+		System.out.println("User: " + user + " è stato aggiunto al db!");
+		return user;
+	}
+
+	@GetMapping("/additionalHeadersExample")
+	public ResponseEntity<User> additionalHeadersExample() {
+
+		// Creo degli headers custom
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("customHeader", "customValue");
+
+		User body = new User(5, "Claudio", "Bisio");
+
+		return new ResponseEntity<User>(body, headers, HttpStatus.OK); // costruttore con 3 parametri: body, headers,
+																		// statuscode
 
 	}
 }
