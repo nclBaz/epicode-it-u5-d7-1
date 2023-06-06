278c54cb6c345763b2a9b770d6f3025f9f94b302
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-06-06 10:31:44 +0200

    Adds Controller examples

---
+++ src/main/java/riccardogulin/u5d7/controllers/ExamplesController.java
@@ -0,0 +1,72 @@
+package riccardogulin.u5d7.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import riccardogulin.u5d7.entities.User;
+
+@RestController
+@RequestMapping("/examples") // prefisso per tutti gli endpoint di questo controller
+public class ExamplesController {
+
+	@GetMapping("/index")
+	public String index() {
+		return "<h1>Pagina principale</h1>";
+	}
+
+	@GetMapping("/about")
+	public String about() {
+		return "<h1>Pagina about</h1>";
+	}
+
+	@GetMapping("/queryParamsExample")
+	public String queryParams(@RequestParam(required = false) String name,
+			@RequestParam(required = false) String surname) {
+		return "Hello " + name + " " + surname;
+	}
+
+	@GetMapping("/pathParamsExample/{parameter}")
+	public String pathParams(@PathVariable String parameter) {
+		return "Ciao sei sull'endpoint /pathParamsExample/" + parameter;
+	}
+
+	@GetMapping("/pathParamsExample")
+	public String noPathParams() {
+		return "Ciao sei sull'endpoint /pathParamsExample/";
+	}
+
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		List<User> users = new ArrayList<User>();
+		users.add(new User(1, "Aldo", "Baglio"));
+		users.add(new User(2, "Giovanni", "Storti"));
+		users.add(new User(3, "Giacomo", "Poretti"));
+		return users;
+	}
+
+	@GetMapping("/users/{id}")
+	public Optional<User> getSingleUser(@PathVariable String id) {
+		List<User> users = new ArrayList<User>();
+		users.add(new User(1, "Aldo", "Baglio"));
+		users.add(new User(2, "Giovanni", "Storti"));
+		users.add(new User(3, "Giacomo", "Poretti"));
+
+		User u = null;
+
+		for (User user : users) {
+			if (Integer.toString(user.getId()).equals(id)) {
+				u = user;
+			}
+		}
+
+		return Optional.ofNullable(u);
+
+	}
+}
