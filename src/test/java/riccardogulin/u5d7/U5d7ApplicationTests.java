671deee1a2ccac4496abbe3775bdac7516db1fca
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-06-06 13:08:39 +0200

    Adds JUnit example

--- src/test/java/riccardogulin/u5d7/U5d7ApplicationTests.java
+++ src/test/java/riccardogulin/u5d7/U5d7ApplicationTests.java
@@ -1,13 +1,23 @@
 package riccardogulin.u5d7;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class U5d7ApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void shouldGetUsersList() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/users")).andExpect(status().is5xxServerError());
 	}
 
 }
